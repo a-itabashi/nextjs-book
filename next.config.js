1845e44d404367c286b08e433b4c826df39eaab0
Aguru itabashi <itabashi.a@gmail.com>
2022-09-25 16:49:36 +0900

    component作成

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   compiler: (() => {
     let compilerConfig = {
       // styledComponentsの有効化
@@ -18,6 +17,16 @@ const nextConfig = {
 
     return compilerConfig
   })(),
+  async rewrites() {
+    return [
+      {
+        // ex. /api/proxy
+        source: `${process.env.NEXT_PUBLIC_API_BASE_PATH}/:match*`,
+        // ex. http://localhost:8000
+        destination: `${process.env.API_BASE_URL}/:match*`,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import { SWRConfig } from 'swr'
+import GlobalSpinner from 'components/organisms/GlobalSpinner'
+import { AuthContextProvider } from 'contexts/AuthContext'
+import GlobalSpinnerContextProvider from 'contexts/GlobalSpinnerContext'
+import { ShoppingCartContextProvider } from 'contexts/ShoppingCartContext'
+import { theme } from 'themes'
+import type { ApiContext } from 'types'
+import { fetcher } from 'utils'
 
 // グローバルのスタイル
 const GlobalStyle = createGlobalStyle`
@@ -13,19 +20,26 @@ textarea {
   font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
     Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
 }
+
 * {
   box-sizing: border-box;
 }
+
 a {
   cursor: pointer;
   text-decoration: none;
   transition: .25s;
-  color: #000;
+  color: ${theme.colors.black};
 }
+
 ol, ul {
   list-style: none;
 }
 `
+
+const context: ApiContext = {
+  apiRootUrl: process.env.NEXT_PUBLIC_API_BASE_PATH || '/api/proxy',
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
@@ -51,7 +65,23 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         <meta name="twitter:site" content="@truck2hand" /> */}
       </Head>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <SWRConfig
+          value={{
+            shouldRetryOnError: false,
+            fetcher,
+          }}
+        >
+          <GlobalSpinnerContextProvider>
+            <ShoppingCartContextProvider>
+              <AuthContextProvider context={context}>
+                <GlobalSpinner />
+                <Component {...pageProps} />
+              </AuthContextProvider>
+            </ShoppingCartContextProvider>
+          </GlobalSpinnerContextProvider>
+        </SWRConfig>
+      </ThemeProvider>
     </>
   )
 }
